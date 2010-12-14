32729fc8cf93d78a5ef535700961090af32f7a97
ch33kybutt <ch33kybutt@yahoo.com>
2010-12-14 10:22:13 +0800

    bfs318-330

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1019,9 +1019,6 @@ struct task_struct {
 
 	unsigned int policy;
 	cpumask_t cpus_allowed;
-#if defined(CONFIG_HOTPLUG_CPU) && defined(CONFIG_SCHED_BFS)
-	cpumask_t unplugged_mask;
-#endif
 
 #ifdef CONFIG_PREEMPT_RCU
 	int rcu_read_lock_nesting;
@@ -1311,7 +1308,7 @@ static inline void tsk_cpus_current(struct task_struct *p)
 
 static inline void print_scheduler_version(void)
 {
-	printk(KERN_INFO"BFS CPU scheduler v0.318 by Con Kolivas.\n");
+	printk(KERN_INFO"BFS CPU scheduler v0.330 by Con Kolivas.\n");
 }
 
 static inline int iso_task(struct task_struct *p)
